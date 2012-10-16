04cec850dbc156beb5550d614fe704656ad166eb
guilhermeanselmo <guilherme.joseanselmo@gmail.com>
2012-10-16 08:56:53 -0300

    Lista os alunos que obtiveram nota >= 6

--- AulaPrática01/aprovados.php
+++ AulaPrática01/aprovados.php
@@ -1,5 +1,6 @@
 <?php
 
+
 $db = pg_connect("host=10.3.1.52 dbname=AulaSD user=postgres password=123456 port=5432");
 if($db){
     $result = pg_query($db, "SELECT * FROM \"Alunos\" WHERE \"Nota\">=6;");
